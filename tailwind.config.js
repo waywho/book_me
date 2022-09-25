5076b24124228f8cb62d8b97cf7af289f3fda52e
WeiHsi Hu <walzerfan@yahoo.com>
2022-09-25 17:22:36 +0200

    Add daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,27 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
     './app/javascript/**/*.js',
-    './config/initializers/simple_form_tailwind.rb'
-  ]
+    './config/initializers/simple_form_tailwind.rb',
+    './node_modules/flowbite/**/*.js'
+  ],
+  daisyui: {
+      themes: [
+        {
+          mytheme: {
+            "primary": "#4ade80",
+            "secondary": "#8b5cf6",
+            "accent": "#fde047",
+            "neutral": "#3D4451",
+            "base-100": "#FFFFFF",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
+          },
+        },
+      ],
+    },
+  plugins: [
+   require("daisyui")
+  ],
 }
