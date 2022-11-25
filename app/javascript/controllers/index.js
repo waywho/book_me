167aacea7103e7b6e097c41a34872cfaf88bc012
WeiHsi Hu <walzerfan@yahoo.com>
2022-11-25 18:01:13 +0100

    Add events component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+// Import view component stimulus controllers
+import "../../components"
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module BookMe
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en de]
 
+    # ViewComponent
     config.view_component.generate.sidecar = true
+    config.view_component.default_preview_layout = "component_preview"
   end
 end
